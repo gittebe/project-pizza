bdfa64bfb6f73e012b471296f1c3a829829da110
Gitte Beckmann <gittebeckmann@hotmail.com>
2024-09-04 08:57:50 +0200

    - added a prompt asking for the user name; - added an alert welcoming the user; - added a prompt to asked user to select one food option out of three; - added an alert to confirm the selected food option;

--- code/script.js
+++ code/script.js
@@ -1,16 +1,39 @@
-// Start here
 
-// Step 1 - Welcome and introduction
-// Your code goes here
+
+// Welcome 
 alert(
   `Welcome to our Javascript Pizzeria. Ready to Start? - Click 'OK' to begin.`
 )
 
-// Step 2 - Food choice
-// Your code goes here
+//Ask for the name
+const name = prompt(`Please enter your name:`)
 
-// Step 3 - Subtype choice
-// Your code goes here
+//Introduction
+alert(
+  `Hallo, ${name.toUpperCase()} Are you ready to order? - Click 'OK'.`
+)
+
+// Submit food choice
+
+const foodChoice = prompt(`What would you like to order? Please enter the number: 
+  1 - Pizza
+  2 - Pasta
+  3 - Salad`)
+
+//The first order
+
+if (foodChoice == 1) {
+  alert(`You like to order Pizza. Great choice`)
+}
+else if (foodChoice == 2) {
+  alert(`Pasta, good choice`)
+}
+else if (foodChoice == 3) {
+  alert(`A salad for you today`)
+}
+
+
+
 
 // Step 4 - Age
 // Your code goes here
