66c1b98b5b13e4a32c397b7616931215b60b538a
Gitte Beckmann <gittebeckmann@hotmail.com>
2024-09-06 19:53:26 +0200

    - added the second order options - changed the order option to drinks - added age - added order confirmation

--- code/switch.js
+++ code/switch.js
@@ -1,23 +1,66 @@
+// Start
 alert(`Please click 'ok' to start your order.`)
 
+// Getting the name
 let name = prompt(`Please enter you name:`)
 
-let firstOrder = prompt(`Hi ${name}, welcome to the Pizzeria. Let's start with your order! What would you like to eat? 
-    1 - Pizza
-    2 - Pasta
-    3 - Salad
+// First order
+let firstOrder = prompt(`Hi ${name}, welcome to the Café. Let's start with your order! What would you like to drink? 
+    1 - Coffee
+    2 - Limonade
+    3 - Juice
     Please enter the number!`)
 
-switch (firstOrder()) {
+// Second order
+switch (firstOrder) {
     case '1':
-        alert(`You have selected Pizza.`)
-        break;
+        secondOrder = prompt(`You have selected Coffee. Please select the Coffee by writing the assigned number:
+            1 - Café Latte Macchiato
+            2 - Espresso
+            3 - Cappuccino`)
+        break
     case '2':
-        alert('You have selected Pasta')
-        break;
+        secondOrder = prompt(`You have selected Limonade. Please select the Limonade by typing 1, 2 or 3:
+            1 - Coke
+            2 - Fanta
+            3 - Sprite`)
+        break
     case '3':
-        alert(`You have selected Salad`)
-        break;
+        secondOrder = prompt(`You have selected Juice. Please select the Juice:
+            1 - Apple Juice
+            2 - Tomato Juice
+            3 - Orange Juice`)
+        break
+
     default:
-        alert(`Sorry, we don't have that`)
+        alert(`Sorry, something went wrong. Please refresh and try again`)
+        break
 }
+
+let age = prompt(`How old are you?`)
+
+// Order confirmation
+let orderConfirmation = ''
+if (firstOrder === '1') {
+    if (secondOrder === '1') orderConfirmation = 'Café Latte Macciato'
+    else if (secondOrder === '2') orderConfirmation = 'Espresso'
+    else if (secondOrder === '3') orderConfirmation = 'Cappuccino'
+} else if (firstOrder === '2') {
+    if (secondOrder === '1') orderConfirmation = 'Coke'
+    else if (secondOrder === '2') orderConfirmation = 'Fanta'
+    else if (secondOrder === '3') orderConfirmation = 'Sprite'
+} else if (firstOrder === '3') {
+    if (secondOrder === '1') orderConfirmation = 'Apple Juice'
+    else if (secondOrder === '2') orderConfirmation = 'Tomato Juice'
+    else if (secondOrder === '3') orderConfirmation = 'Orange Juice'
+}
+
+// Ask for age of the user & send out confirmation of order
+if (age <= 13) {
+    alert(`You like to order a small ${orderConfirmation}. Click 'ok' to confirm your order!`)
+} else {
+    alert(`You like to order a big ${orderConfirmation}. Click 'ok' to confirm your order`)
+}
+
+
+alert(`Thank you for your order!`)
